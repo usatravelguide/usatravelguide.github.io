60c1bce9ac41fcd2c5d4b5ce66a3cd0fcb49bb9f
usatravelguide <119908067+usatravelguide@users.noreply.github.com>
2023-01-17 23:33:41 -0500

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,24 @@
+function displaySearch() {
+	$('#SearchList').show();
+}
+
+function hideSearch() {
+ 	$('#SearchList').hide();
+}
+
+function searchFilterFunction() {
+  var input, filter, ul, li, a, i;
+  
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("searchDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
