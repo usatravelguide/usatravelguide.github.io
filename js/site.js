282c95f30478175e58966b23aa8243199f89291b
usatravelguide <119908067+usatravelguide@users.noreply.github.com>
2023-02-07 20:05:04 -0500

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -15,7 +15,8 @@ function searchFilterFunction() {
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//    if (txtValue.toUpperCase().indexOf(filter) > -1) {  // makes search work as "contains"
+    if (txtValue.toUpperCase().startsWith(filter)) {
       a[i].style.display = "";
     } else {
       a[i].style.display = "none";
