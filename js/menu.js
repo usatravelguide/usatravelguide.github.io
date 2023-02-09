7f225f6497e75a833576ddf8a7f447c2f6f628d5
usatravelguide <119908067+usatravelguide@users.noreply.github.com>
2023-02-08 21:51:58 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,51 @@
+$("#topmenustrip").html('<ul class="nav nav-tabs topmenu">' +
+'	  <li class="nav-item dropdown">' + 
+'		<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">USA Travel Guide</a>' + 
+'		<div class="dropdown-menu">' + 
+'			<h6 class="dropdown-header ddheader">Asia</h6>' + 
+'			<a class="dropdown-item" href="china.html">China</a>' + 
+'			<a class="dropdown-item" href="japan.html">Japan</a>' + 
+'			<a class="dropdown-item" href="northkorea.html">North Korea</a>' + 
+'			<a class="dropdown-item" href="southkorea.html">South Korea</a>' + 
+'			<a class="dropdown-item" href="taiwan.html">Taiwan</a>' + 
+'			<a class="dropdown-item" href="vietnam.html">Vietnam</a>' + 
+'			<a class="dropdown-item" href="Philippines.html">Philippines</a>' + 
+'			<a class="dropdown-item" href="malaysia.html">Malaysia</a>' + 
+'			<a class="dropdown-item" href="india.html">India</a>' + 
+'			<a class="dropdown-item" href="singapore.html">Singapore</a>' + 
+'			<div class="dropdown-divider"></div>' + 
+'			<h6 class="dropdown-header ddheader">Europe</h6>' + 
+'			<a class="dropdown-item" href="unitedkingdom.html">United Kingdom</a>' + 
+'			<a class="dropdown-item" href="spain.html">Spain</a>' + 
+'			<a class="dropdown-item" href="france.html">France</a>' + 
+'			<a class="dropdown-item" href="portugal.html">Portugal</a>' + 
+'			<a class="dropdown-item" href="ireland.html">Ireland</a>' + 
+'			<a class="dropdown-item" href="italy.html">Italy</a>' + 
+'			<a class="dropdown-item" href="switzerland.html">Switzerland</a>' + 
+'' + 
+'		</div>' + 
+'	  </li>' + 
+'	  </li>' + 
+'	</ul>' + 
+'	<div id="searchDropdown" class="search-dropdown-content">' + 
+'		<input type="text" placeholder="Search.." id="searchInput" onkeyup="searchFilterFunction()" onclick="displaySearch()" onfocusout="setTimeout(hideSearch,500)">' + 
+'		<div id="SearchList" class="search-list">' + 
+'			<a href="china.html">China</a>' + 
+'			<a href="france.html">France</a>' + 
+'			<a href="india.html">India</a>' + 
+'			<a href="ireland.html">Ireland</a>' + 
+'			<a href="italy.html">Italy</a>' + 
+'			<a href="japan.html">Japan</a>' + 
+'			<a href="malaysia.html">Malaysia</a>' + 
+'			<a href="northkorea.html">North Korea</a>' + 
+'			<a href="philippines.html">Philippines</a>' + 
+'			<a href="portugal.html">Portugal</a>' + 
+'			<a href="singapore.html">Singapore</a>' + 
+'			<a href="southkorea.html">South Korea</a>' + 
+'			<a href="spain.html">Spain</a>' + 
+'			<a href="switzerland.html">Switzerland</a>' + 
+'			<a href="taiwan.html">Taiwan</a>' + 
+'			<a href="unitedkingdom.html">United Kingdom</a>' + 
+'			<a href="vietnam.html">Vietnam</a>' + 
+'		</div>' + 
+'	</div>');
